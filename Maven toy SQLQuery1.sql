b5fbd7f6886af08a9a8870ecbfb54ce90a8d0096
A-Jeeva <85949588+A-Jeeva@users.noreply.github.com>
2021-06-15 23:28:52 +0530

    Update Maven toy SQLQuery1.sql

--- Maven toy SQLQuery1.sql
+++ Maven toy SQLQuery1.sql
@@ -1,4 +1,11 @@
-Use [Mexico Toy Sales]
+/*  Sales and inventory data of toy store - Data exploration
+
+Skills used - Joins, Temp Tables, Aggregate Functions, Converting Data Type
+
+*/
+
+
+Use [Maven Toy Sales]
 
 ---- Total Products and Stores
 
@@ -7,6 +14,9 @@ from products
 
 Select Count(Store_ID) as Total_Stores
 from stores
+
+
+
 
 ---- Adding Profit Column to show profit for each product
 
@@ -20,20 +30,54 @@ Update products
 set Products.Product_Profit = products.Product_Price-products.Product_Cost
 
 
---- Total profit as per Product categories 
 
-Select 
-Coalesce(pr.Product_Category,'Grand Total') as Product_Category,
+
+----  Calculate the percent of total sales and profit contributed by each product category
+
+If OBJECT_ID (N'tempdb..#temp_Overallpercent') is not Null
+Begin
+Drop table #temp_Overallpercent
+End
+
+Create table #temp_Overallpercent
+(
+Product_Category varchar(50),
+Total_sales float,
+Overall_sales float,
+Total_profit float,
+Overall_profit float
+)
+
+Insert into #temp_Overallpercent
+Select pr.Product_Category,
 cast(sum(Pr.Product_Price*sa.Units) as int) as Total_Sales,
-cast(sum(Pr.Product_Cost*sa.Units) as int) as Total_Cost,
+
+(select cast(sum(Pr.Product_Price*sa.Units) as int) as Total_Sales
+From products pr
+join sales sa
+on pr.Product_ID=sa.Product_ID),
+
 cast(sum(Pr.Product_Profit*sa.Units) as int) as Total_Profit,
-sum(sa.units) as Total_units_Sold
+
+(select cast(sum(Pr.Product_Profit*sa.Units) as int) as Total_Profit
+From products pr
+join sales sa
+on pr.Product_ID=sa.Product_ID)
+
 From products pr
 join sales sa
 on pr.Product_ID=sa.Product_ID
-Group by Rollup (pr.Product_Category)
-Order by 
-(case When pr.Product_Category is null then 1 else 2 End) Desc,Total_Profit desc
+Group by pr.Product_Category
+
+Select Product_category, 
+Total_sales,
+Round((Total_sales/Overall_sales)*100,2) as  'Salespercent%',
+Total_profit,
+Round((Total_profit/overall_profit)*100,2) as 'Profitpercent%'
+from #temp_Overallpercent
+Order by [Profitpercent%] desc
+
+
 
 
 ----Top 10 products based on profit and Total units sold in particular product
@@ -47,6 +91,7 @@ join sales sa
 on pr.Product_ID=sa.Product_ID
 Group by pr.Product_Name
 Order by Total_Sales Desc
+
 
 
 
@@ -64,6 +109,9 @@ on sa.Store_ID=st.Store_ID
 Group by st.Store_Location,pr.Product_Category
 Order by st.Store_Location,Total_Profit Desc
 
+
+
+
 --Top 10 Store city in Total Sales and profit 
 
 Select Top(10) st.Store_City,
@@ -77,7 +125,9 @@ join stores st
 on sa.Store_ID=st.Store_ID
 Group by st.Store_City
 Order by Total_Sales Desc
- 
+
+
+
 
 ---- No of stores with Zero Stocks with Product category---- 
 
@@ -90,6 +140,8 @@ on pr.Product_ID= inv.Product_ID
 Where inv.Stock_On_Hand = 0
 Group by st.Store_Location,pr.Product_Category
 order by st.Store_Location,Store_count_W_zerostock desc
+
+
 
 
 ----How much money is tied up in inventory at the toy stores
@@ -105,6 +157,8 @@ Group by st.Store_Location,pr.Product_Category,pr.Product_Name
 order by st.Store_Location,Inventory_Cost desc, Total_Stock desc
 
 
+
+
 ---- First 5 stores opened ----
 
 Select Top(5) Store_Name,cast(Max(Store_Open_Date) as date) as Opendate,
@@ -116,6 +170,7 @@ join products pr
 on sa.Product_ID=pr.Product_ID
 Group by Store_Name
 order by Opendate 
+
 
 
 
